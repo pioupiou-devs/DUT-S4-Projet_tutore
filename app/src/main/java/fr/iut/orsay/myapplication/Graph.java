5cdc5ee96ef9075c85ef3d5d3c070add27a609cd
zeyzen <64972509+zeyzen@users.noreply.github.com>
2021-05-07 16:43:44 +0200

    modification attribute of graph: delete curve label, dataSets change from ArrayList<ArrayList<Entry>> type to ArrayList<ILineDataSet>
    definition of method print and printdataset
    creation of printer() (use in the prints methods)
    
    beginning of work on update, rename, remove and addDataSet,
    
    in Main activity, some modification to test new method

--- app/src/main/java/fr/iut/orsay/myapplication/Graph.java
+++ app/src/main/java/fr/iut/orsay/myapplication/Graph.java
@@ -1,128 +1,221 @@
 package fr.iut.orsay.myapplication;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 
-public class Graph implements GraphInterface
-    {
+public class Graph implements GraphInterface {
+    
+    int height = 0, width = 0, initHeight = 0, initWidth = 0, compoWidth = 0, compoHeight = 0;
+    String graphName;
+    //ArrayList<String> labels = new ArrayList<>(); ///on stock toutes les noms des courbes
+    //ArrayList<ArrayList<Entry>> dataSets = new ArrayList<>(); //on stickes les valeurs?
+    ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+    
+    LineChart chart = null;
+    
+    public Graph(String graphName, int compoWidth, int compoHeight, int width, int height) {
+        this.graphName = graphName;
+        this.compoWidth = compoWidth;
+        this.compoHeight = compoHeight;
+        this.width = width;
+        this.height = height;
+        this.initWidth = width;
+        this.initHeight = height;
         
-        int height = 0, width = 0, initHeight = 0, initWidth = 0, compoWidth = 0, compoHeight = 0;
-        String graphName;
-        ArrayList<String> labels = new ArrayList<>();
-        ArrayList<ArrayList<Entry>> dataSets = new ArrayList<>();
-        LineChart chart = null;
-        
-        public Graph(String graphName, int compoWidth, int compoHeight, int width, int height)
-            {
-                this.graphName = graphName;
-                this.compoWidth = compoWidth;
-                this.compoHeight = compoHeight;
-                this.width = width;
-                this.height = height;
-                this.initWidth = width;
-                this.initHeight = height;
-                
-                //TODO: create the graph => chart
-                chart.setPinchZoom(true);
-            }
-        
-        public Graph(String graphName, int compoWidth, int compoHeight, int width, int height, ArrayList<String> labels, ArrayList<ArrayList<Entry>> dataSets)
-            {
-                this.graphName = graphName;
-                this.compoWidth = compoWidth;
-                this.compoHeight = compoHeight;
-                this.width = width;
-                this.height = height;
-                this.initWidth = width;
-                this.initHeight = height;
-                this.labels = labels;
-                this.dataSets = dataSets;
-                
-                //TODO: create the graph => chart
-                chart.setPinchZoom(true);
-            }
-        
-        @Override public void addDataSet(String label, ArrayList<Entry> dataSet)
-            {
-            
-            }
-        
-        @Override public void removeDataSet(String label)
-            {
-            
-            }
-        
-        @Override public void show()
-            {
-            
-            }
-        
-        @Override public void showDataSet(String label)
-            {
-            
-            }
-        
-        @Override public void zoomIn()
-            {
-                width -= (initWidth * 10) / 100;
-                height -= (initHeight * 10) / 100;
-                update();
-            }
-        
-        @Override public void zoomIn(int scale)
-            {
-                width -= (initWidth * scale) / 100;
-                height -= (initHeight * scale) / 100;
-                update();
-            }
-        
-        @Override public void zoomIn(int scaleW, int scaleH)
-            {
-                width -= (initWidth * scaleW) / 100;
-                height -= (initHeight * scaleH) / 100;
-                update();
-            }
-        
-        @Override public void zoomOut()
-            {
-                width += (initWidth * 10) / 100;
-                height += (initHeight * 10) / 100;
-                update();
-            }
-        
-        @Override public void zoomOut(int scale)
-            {
-                width += (initWidth * scale) / 100;
-                height += (initHeight * scale) / 100;
-                update();
-            }
-        
-        @Override public void zoomOut(int scaleW, int scaleH)
-            {
-                width += (initWidth * scaleW) / 100;
-                height += (initHeight * scaleH) / 100;
-                update();
-            }
-        
-        @Override public void rename(String newLabel)
-            {
-            
-            }
-        
-        @Override public void update()
-            {
-                chart.zoomToCenter(width, height);
-            }
-        
-        @Override public String print()
-            {
-                return null;
-            }
-        
-        @Override public String printDataSet(String label)
-            {
-                return null;
-            }
+        //TODO: create the graph => chart
+        chart.setPinchZoom(true);
     }
+    
+    //public Graph(String graphName, int compoWidth, int compoHeight, int width, int height, ArrayList<String> labels, ArrayList<ArrayList<Entry>> dataSets) {
+    public Graph(String graphName, int compoWidth, int compoHeight, int width, int height, ArrayList<ILineDataSet> dataSets) {
+        this.graphName = graphName;
+        this.compoWidth = compoWidth;
+        this.compoHeight = compoHeight;
+        this.width = width;
+        this.height = height;
+        this.initWidth = width;
+        this.initHeight = height;
+        //this.labels = labels;
+        this.dataSets = dataSets;
+        
+        //TODO: create the graph => chart
+        this.chart=create_chart();
+        chart.setPinchZoom(true);
+    }
+    
+    
+    public LineChart create_chart() {//creer le cadre et initialiser les valeur du cadre
+        //TODO : finir la fonction et regarder comment creer un linechart propre
+        LineChart new_chart = null;
+        
+        return new_chart;
+    }
+    
+    @Override
+    public void addDataSet(String label, ArrayList<Entry> dataSet) {//ajouter les entry dans le tableau
+        //LineDataSet temp = new LineDataSet(dataSet,label);
+        //TODO : ajouter les anticon --> verif sur le label/ entry vide
+        //4 courbes max
+        if (dataSets.size() == 4) { //on ne veux pas plus de 4 courbes dans notre appli
+            System.out.println("max number of data set for " + graphName + " (max is 4)");
+            return;
+        }
+        
+        dataSets.add(new LineDataSet(dataSet, label));
+        
+        return;
+    }
+    
+    @Override
+    public void removeDataSet(String label) { //clear le tablea I gess, de une seule courbe
+        
+        for (int i = 0; i < dataSets.size(); i++) { //pas de recherche avencer car on aurais au max 4 courbes
+            if (dataSets.get(i).getLabel() == label) { //on cherche par rapport au label
+                dataSets.remove(i);
+                return;
+            }
+        }
+        
+        return;
+    }
+    
+    @Override
+    public void show() { // affiche toute les courbes et le graph
+        
+        
+        update();
+        return;
+    
+    }
+    
+    @Override
+    public void showDataSet(String label) {//affiche une des courbes graphique et desaffiche les autres
+        
+        
+        
+        update();
+        return;
+    }
+    
+    @Override
+    public void zoomIn() {
+        width -= (initWidth * 10) / 100;
+        height -= (initHeight * 10) / 100;
+        update();
+    }
+    
+    @Override
+    public void zoomIn(int scale) {
+        width -= (initWidth * scale) / 100;
+        height -= (initHeight * scale) / 100;
+        update();
+    }
+    
+    @Override
+    public void zoomIn(int scaleW, int scaleH) {
+        width -= (initWidth * scaleW) / 100;
+        height -= (initHeight * scaleH) / 100;
+        update();
+    }
+    
+    @Override
+    public void zoomOut() {
+        width += (initWidth * 10) / 100;
+        height += (initHeight * 10) / 100;
+        update();
+    }
+    
+    @Override
+    public void zoomOut(int scale) {
+        width += (initWidth * scale) / 100;
+        height += (initHeight * scale) / 100;
+        update();
+    }
+    
+    @Override
+    public void zoomOut(int scaleW, int scaleH) {
+        width += (initWidth * scaleW) / 100;
+        height += (initHeight * scaleH) / 100;
+        update();
+    }
+    
+    @Override
+    public void rename(String newLabel) { //renomer dans la listes des grphique??
+        this.graphName = newLabel;
+        return;
+    }
+    
+    @Override
+    public void update() { //met a jour le graph zoom ou des courbes en plus etc
+        
+        //TODO : gerer les truc pour update les courbes
+        
+        
+        chart.zoomToCenter(width, height);
+        
+        return;
+    }
+    
+    @Override
+    public String print() { // affiche dans la console, toute les courbes
+        String out ="";
+    
+        out+=("Data form graph "+graphName+"\n");
+        
+        for (int i = 0; i < dataSets.size(); i++) {
+            out+=printer(dataSets.get(i));
+            out+="...\n";
+        }
+        
+        out+=("--------------------------------");
+        
+        return out;
+    }
+    
+    
+    @Override
+    public String printDataSet(String label) { //affiche une seule courbe
+        String out ="";
+        boolean found=false;
+    
+        //out+=("Data set form dataset "+label+"\n");
+        
+        for (int i = 0; i < dataSets.size(); i++) { //pas de recherche avencer car on aurais au max 4 courbes
+            if (dataSets.get(i).getLabel() == label) { //on cherche par rapport au label
+                out+=printer(dataSets.get(i));
+                found = true;
+            }
+        }
+        
+        if (!found){
+            out+=("No data set found for the search :"+label+"\n");
+        }
+    
+        out+=("--------------------------------");
+        return out;
+    }
+    
+    //sert a mettre en forme les coubres
+    private String printer(ILineDataSet data){
+        String out ="";
+        
+        out+=("Data set : "+data.getLabel()+"\n");
+        
+        for (int i =0; i<data.getEntryCount();i++) {
+            Entry temp = data.getEntryForIndex(i);
+            
+            out+=("\t - x :"+temp.getX()+"\t y :"+temp.getY()+"\n");
+        }
+        
+        return out;
+    }
+}
--- app/src/main/java/fr/iut/orsay/myapplication/MainActivity.java
+++ app/src/main/java/fr/iut/orsay/myapplication/MainActivity.java
@@ -3,8 +3,11 @@ package fr.iut.orsay.myapplication;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -20,6 +23,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity
     {
     
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override protected void onCreate(Bundle savedInstanceState)
             {
                 super.onCreate(savedInstanceState);
@@ -31,12 +35,14 @@ public class MainActivity extends AppCompatActivity
                 ArrayList<Entry> values = new ArrayList<>();
                 values.add(new Entry(1, 50));
                 values.add(new Entry(2, 30));
-                values.add(new Entry(3, 100));
-                values.add(new Entry(5, 12));
-                values.add(new Entry(7, 98));
-                values.add(new Entry(8, 42));
-                values.add(new Entry(6, 33));
+                values.add(new Entry(3, 35));
                 values.add(new Entry(4, 66));
+                values.add(new Entry(5, 65));
+                values.add(new Entry(6, 47));
+                values.add(new Entry(7, 54));
+                values.add(new Entry(8, 42));
+                
+                
                 
                 LineDataSet set1;
                 if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0)
@@ -50,19 +56,31 @@ public class MainActivity extends AppCompatActivity
                     {
                         set1 = new LineDataSet(values, "Sample Data");
                         set1.setDrawIcons(false);
-                        set1.enableDashedLine(10f, 5f, 0f);
-                        set1.enableDashedHighlightLine(10f, 5f, 0f);
-                        set1.setColor(Color.DKGRAY);
-                        set1.setCircleColor(Color.DKGRAY);
+                        
+                        
+                        //set1.enableDashedLine(10f, 5f, 0f);
+                        
+                        //set1.enableDashedHighlightLine(10f, 5f, 0f);
+                        
+                        /*//to make the smooth line as the graph is adrapt change so smooth curve
+                        set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+                        //to enable the cubic density : if 1 then it will be sharp curve
+                        set1.setCubicIntensity(0.2f);*/
+                        
+                        
+                        set1.setColor(Color.BLACK);
+                        set1.setCircleColor(Color.BLACK);
                         set1.setLineWidth(1f);
                         set1.setCircleRadius(3f);
                         set1.setDrawCircleHole(false);
                         set1.setValueTextSize(9f);
-                        set1.setDrawFilled(true);
+                        //set1.setDrawFilled(true);
+                        set1.setDrawFilled(false);
                         set1.setFormLineWidth(1f);
                         set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
                         set1.setFormSize(15.f);
-                        if (Utils.getSDKInt() >= 18)
+                        
+                        /*if (Utils.getSDKInt() >= 18)
                             {
                                 Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_blue);
                                 set1.setFillDrawable(drawable);
@@ -70,14 +88,20 @@ public class MainActivity extends AppCompatActivity
                         else
                             {
                                 set1.setFillColor(Color.DKGRAY);
-                            }
+                            }*/
                         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
                         dataSets.add(set1);
+                        
+                        mChart.setDrawBorders(true); //encadre la graph
+                        
+                        // don't forget to refresh the drawing
+                        //chart.invalidate();
+                        
                         LineData data = new LineData(dataSets);
                         mChart.setData(data);
                     }
                // LineDataSet set1;
-                if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0)
+                /*if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0)
                     {
                         set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
                         set1.setValues(values);
@@ -109,10 +133,56 @@ public class MainActivity extends AppCompatActivity
                             {
                                 set1.setFillColor(Color.DKGRAY);
                             }
+                        
+                        
                         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
                         dataSets.add(set1);
+                        
                         LineData data = new LineData(dataSets);
+                        
                         mChart.setData(data);
                     }
+                    
+                 */
+                
+                
+                Graph test = new Graph("graph de test", 5, 5, 5, 5);
+    
+                ArrayList<Entry> val = new ArrayList<>();
+                val.add(new Entry(1, 50));
+                val.add(new Entry(2, 30));
+                val.add(new Entry(3, 35));
+                val.add(new Entry(4, 66));
+                val.add(new Entry(5, 65));
+                val.add(new Entry(6, 47));
+                val.add(new Entry(7, 54));
+                val.add(new Entry(8, 42));
+    
+                ArrayList<Entry> val2 = new ArrayList<>();
+                val2.add(new Entry(1, 12));
+                val2.add(new Entry(2, 65));
+                val2.add(new Entry(3, 78));
+                val2.add(new Entry(4, 98));
+                val2.add(new Entry(5, 65));
+                val2.add(new Entry(6, 78));
+                val2.add(new Entry(7, 64));
+                val2.add(new Entry(8, 78));
+                
+                String name1= "erste Kurve";
+                String name2= "zweiten Kurve";
+                
+                test.addDataSet(name1,val);
+                test.addDataSet(name2,val2);
+    
+                System.out.println("####################################################################");
+                
+                System.out.println(test.printDataSet(name1));
+                System.out.println("####################################################################");
+                System.out.println(test.printDataSet("error test"));
+                System.out.println("####################################################################");
+                System.out.println(test.print());
+    
+                System.out.println("####################################################################");
+                
             }
     }

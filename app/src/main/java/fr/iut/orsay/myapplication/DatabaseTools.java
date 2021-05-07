3e3b76b901248613a8daa6f125267f5073132876
madocl <madoc.lefevre@gmail.com>
2021-05-07 15:08:19 +0200

    ajout fonctions dans DatabaseTools.java + ajout requêtes

--- app/src/main/java/fr/iut/orsay/myapplication/DatabaseTools.java
+++ app/src/main/java/fr/iut/orsay/myapplication/DatabaseTools.java
@@ -1,5 +1,6 @@
 package fr.iut.orsay.myapplication;
 
+import android.content.Context;
 import android.os.Build;
 import android.os.StrictMode;
 
@@ -10,6 +11,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
 
 import java.time.LocalDateTime;
@@ -19,32 +21,36 @@ import java.util.ArrayList;
 import com.github.mikephil.charting.data.Entry;
 
 public class DatabaseTools {
-    final static String LOGIN = "root", PASSWORD = null;
-
-    public static Connection openConnection(String url) throws SQLException {
+    public static Connection openConnection(String url) throws SQLException, ClassNotFoundException {
         Connection co = null;
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        Class.forName("org.mariadb.jdbc.Driver");
         co = DriverManager.getConnection(url);
         System.out.println("coucou ! " + co.getMetaData());
         return co;
     }
 
-    public static ArrayList<Entry> getValues(int numSensor, PreparedStatement preparedStatement) throws SQLException {
+    public static ArrayList<Entry> getValues(int numSensor, int numIPSO, PreparedStatement preparedStatement) throws SQLException {
         preparedStatement.setInt(1, numSensor);
+        preparedStatement.setInt(2, numIPSO);
         ResultSet resultSet = preparedStatement.executeQuery();
 
         ArrayList<Entry> values = new ArrayList<>();
+        int i = 1;
         while(resultSet.next()) {
-            values.add(new Entry(Float.parseFloat(resultSet.getString(1)), resultSet.getTimestamp(2))); //TODO : problème de compatibilité entre les Entry et les DateTime
+            //values.add(new Entry(Float.parseFloat(resultSet.getString(1)), resultSet.getTimestamp(2))); //TODO : problème de compatibilité entre les Entry et les DateTime
+            values.add(new Entry(Float.parseFloat(resultSet.getString(1)), i)); //TODO : problème de compatibilité entre les Entry et les DateTime
+            i++;
         }
         return values;
     }
 
-    public static ArrayList<Entry> getValues(int numSensor, PreparedStatement preparedStatement, LocalDateTime dateMin, LocalDateTime dateMax) throws SQLException { //surcharge pour la spécification d'une plage de dates
+    /*public static ArrayList<Entry> getValues(int numSensor, int numIPSO, PreparedStatement preparedStatement, LocalDateTime dateMin, LocalDateTime dateMax) throws SQLException { //surcharge pour la spécification d'une plage de dates
         preparedStatement.setInt(1, numSensor);
+        preparedStatement.setInt(2, numIPSO);
         preparedStatement.setTimestamp(2, Timestamp.valueOf(String.valueOf(dateMax)));
         preparedStatement.setTimestamp(3, Timestamp.valueOf(String.valueOf(dateMin)));
 
@@ -55,5 +61,64 @@ public class DatabaseTools {
             values.add(new Entry(Float.parseFloat(resultSet.getString(1)), resultSet.getTimestamp(2))); //TODO : problème de compatibilité entre les Entry et les DateTime
         }
         return values;
+    }*/
+
+    //call this method when radio button "type" is selected, it returns the list of all types
+    //https://www.tutorialspoint.com/how-can-i-add-items-to-a-spinner-in-android
+    public static ArrayList<Integer> getTypes(Connection co, Context context) throws SQLException {
+        ArrayList<Integer> listTypes = new ArrayList<>();
+
+        Statement statement = co.createStatement();
+        ResultSet resultSet = statement.executeQuery(context.getResources().getString(R.string.get_types));
+
+        while (resultSet.next()){
+            listTypes.add(resultSet.getInt(1));
+        }
+        return listTypes;
     }
+
+    //call this method to get types from a specified sensor
+    public static ArrayList<Integer> getTypes(int numSensor, PreparedStatement preparedStatement) throws SQLException {
+        ArrayList<Integer> listTypes = new ArrayList<>();
+
+        preparedStatement.setInt(1, numSensor);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while(resultSet.next()){
+            listTypes.add(resultSet.getInt(1));
+        }
+        return listTypes;
+    }
+
+    //call this method when radio button "sensor" is selected, it returns the list of all sensors
+    public static ArrayList<Integer> getSensors(Connection co, Context context) throws SQLException{
+        ArrayList<Integer> listSensors = new ArrayList<>();
+
+        Statement statement = co.createStatement();
+        ResultSet resultSet = statement.executeQuery(context.getResources().getString(R.string.get_sensors));
+
+        while (resultSet.next()){
+            listSensors.add(resultSet.getInt(1));
+        }
+        return listSensors;
+    }
+
+    //call this method to get sensors with a specified sensor
+    public static ArrayList<Integer> getSensors(int numIPSO, PreparedStatement preparedStatement) throws SQLException{
+        ArrayList<Integer> listSensors = new ArrayList<>();
+
+        preparedStatement.setInt(1, numIPSO);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            listSensors.add(resultSet.getInt(1));
+        }
+        return listSensors;
+    }
+
+    //sensor (liste de tous les capteurs) et type(les différents types présents sur le google doc "types données"
+    // --> afficher que si y a des données pour ce capteur/ type
+    //si "type" est sélectionné, le champ data list affiche tous les capteurs qui délivrent ce type et qui ont des données
+    //si "sensor" est sélectionné, le champ data list affiche tous les types délivrés par ce capteur et qui ont des données
+
 }
--- app/src/main/java/fr/iut/orsay/myapplication/MainActivity.java
+++ app/src/main/java/fr/iut/orsay/myapplication/MainActivity.java
@@ -25,49 +25,46 @@ import java.sql.SQLException;
 
 import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity
-    {
-        private static final String DATABASE_URL = "jdbc:mariadb://localhost:3306/pt?user=root&password=root";
+public class MainActivity extends AppCompatActivity {
+    private static final String DATABASE_URL = "jdbc:mariadb://78.116.137.76:3306/pt?user=usr1&password=pt1";
 
-        @RequiresApi(api = Build.VERSION_CODES.N)
-        @Override protected void onCreate(Bundle savedInstanceState)
-            {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 0);
 
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 0);
+        LineChart mChart = findViewById(R.id.chart);
+        mChart.setTouchEnabled(true);
+        mChart.setPinchZoom(true);
 
+        CompletableFuture<Connection> databaseConnecting = CompletableFuture.supplyAsync(() -> {
+            Connection co = null;
 
-                LineChart mChart = findViewById(R.id.chart);
-                mChart.setTouchEnabled(true);
-                mChart.setPinchZoom(true);
+            try {
+                co = DatabaseTools.openConnection(DATABASE_URL);
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
 
+            System.out.println("connexion =" + co.toString());
+            return co;
+        });
 
-                //skip-bind-address on mariadb on file "my.ini" ?
-                CompletableFuture<Void> databaseConnecting = CompletableFuture.runAsync(() -> {
-                    Connection co = null;
+        ArrayList<Entry> values = new ArrayList<>();
 
-                    try {
-                        co = DatabaseTools.openConnection(DATABASE_URL);
-                    } catch (SQLException throwables) {
-                        throwables.printStackTrace();
-                    }
+        try {
+            PreparedStatement preparedStatement = databaseConnecting.get().prepareStatement(getResources().getString(R.string.get_values));
+            values = DatabaseTools.getValues(1, 1, preparedStatement);
+        } catch (SQLException | ExecutionException | InterruptedException throwables) {
+            throwables.printStackTrace();
+        }
 
-                    System.out.println("connexion =" + co.toString());
-                });
-
-                PreparedStatement preparedStatement = co.prepareStatement(getResources().getString(R.string.get_values));
-                ArrayList<Entry> values = new ArrayList<>();
-
-                try {
-                    values = DatabaseTools.getValues(1, preparedStatement);
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-
-                values.add(new Entry(1, 50));
+                /*values.add(new Entry(1, 50));
                 values.add(new Entry(2, 30));
                 values.add(new Entry(3, 100));
                 values.add(new Entry(5, 12));
@@ -75,82 +72,72 @@ public class MainActivity extends AppCompatActivity
                 values.add(new Entry(8, 42));
                 values.add(new Entry(6, 33));
                 values.add(new Entry(4, 66));
-                
-                LineDataSet set1;
-                if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0)
-                    {
-                        set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
-                        set1.setValues(values);
-                        mChart.getData().notifyDataChanged();
-                        mChart.notifyDataSetChanged();
-                    }
-                else
-                    {
-                        set1 = new LineDataSet(values, "Sample Data");
-                        set1.setDrawIcons(false);
-                        set1.enableDashedLine(10f, 5f, 0f);
-                        set1.enableDashedHighlightLine(10f, 5f, 0f);
-                        set1.setColor(Color.DKGRAY);
-                        set1.setCircleColor(Color.DKGRAY);
-                        set1.setLineWidth(1f);
-                        set1.setCircleRadius(3f);
-                        set1.setDrawCircleHole(false);
-                        set1.setValueTextSize(9f);
-                        set1.setDrawFilled(true);
-                        set1.setFormLineWidth(1f);
-                        set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
-                        set1.setFormSize(15.f);
-                        if (Utils.getSDKInt() >= 18)
-                            {
-                                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_blue);
-                                set1.setFillDrawable(drawable);
-                            }
-                        else
-                            {
-                                set1.setFillColor(Color.DKGRAY);
-                            }
-                        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-                        dataSets.add(set1);
-                        LineData data = new LineData(dataSets);
-                        mChart.setData(data);
-                    }
-               // LineDataSet set1;
-                if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0)
-                    {
-                        set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
-                        set1.setValues(values);
-                        mChart.getData().notifyDataChanged();
-                        mChart.notifyDataSetChanged();
-                    }
-                else
-                    {
-                        set1 = new LineDataSet(values, "Sample Data");
-                        set1.setDrawIcons(false);
-                        set1.enableDashedLine(10f, 5f, 0f);
-                        set1.enableDashedHighlightLine(10f, 5f, 0f);
-                        set1.setColor(Color.DKGRAY);
-                        set1.setCircleColor(Color.DKGRAY);
-                        set1.setLineWidth(1f);
-                        set1.setCircleRadius(3f);
-                        set1.setDrawCircleHole(false);
-                        set1.setValueTextSize(9f);
-                        set1.setDrawFilled(true);
-                        set1.setFormLineWidth(1f);
-                        set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
-                        set1.setFormSize(15.f);
-                        if (Utils.getSDKInt() >= 18)
-                            {
-                                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_blue);
-                                set1.setFillDrawable(drawable);
-                            }
-                        else
-                            {
-                                set1.setFillColor(Color.DKGRAY);
-                            }
-                        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-                        dataSets.add(set1);
-                        LineData data = new LineData(dataSets);
-                        mChart.setData(data);
-                    }
+                */
+        LineDataSet set1;
+        if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            set1 = new LineDataSet(values, "Sample Data");
+            set1.setDrawIcons(false);
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.DKGRAY);
+            set1.setCircleColor(Color.DKGRAY);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+            if (Utils.getSDKInt() >= 18) {
+                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_blue);
+                set1.setFillDrawable(drawable);
+            } else {
+                set1.setFillColor(Color.DKGRAY);
             }
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+            LineData data = new LineData(dataSets);
+            mChart.setData(data);
+        }
+        // LineDataSet set1;
+        if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            set1 = new LineDataSet(values, "Sample Data");
+            set1.setDrawIcons(false);
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.DKGRAY);
+            set1.setCircleColor(Color.DKGRAY);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+
+            if (Utils.getSDKInt() >= 18) {
+                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_blue);
+                set1.setFillDrawable(drawable);
+            } else {
+                set1.setFillColor(Color.DKGRAY);
+            }
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+            LineData data = new LineData(dataSets);
+            mChart.setData(data);
+        }
     }
+}

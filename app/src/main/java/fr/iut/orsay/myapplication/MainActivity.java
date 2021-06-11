20fd7894845df04570ea633c26528d2056b72e08
69VR69 <44296242+69VR69@users.noreply.github.com>
2021-06-11 16:10:39 +0200

    Update the bottom menu

--- app/src/main/java/fr/iut/orsay/myapplication/MainActivity.java
+++ app/src/main/java/fr/iut/orsay/myapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity
         @Override protected void onCreate(Bundle savedInstanceState)
             {
                 super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
+                setContentView(R.layout.activity_curve);
                 LineChart mChart = findViewById(R.id.chart);
                 mChart.setTouchEnabled(true);
                 mChart.setPinchZoom(true);

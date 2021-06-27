be098c57f071e4dad85f2f8171e4e6f65c03b377
69VR69 <fouques.antonin@gmail.com>
2021-06-27 09:33:27 +0200

    add javadoc to CurveActivity

--- app/src/main/java/fr/iut/orsay/myapplication/CurveActivity.java
+++ app/src/main/java/fr/iut/orsay/myapplication/CurveActivity.java
@@ -12,57 +12,69 @@ import java.util.Objects;
 public class CurveActivity extends AppCompatActivity
     {
         public static Graph selectedGraph;
+        //Listener sur les élements de la barre de navigation
         private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item ->
         {
-            System.out.println(item);
+            //si nous avons clicker sur le bouton list
             if (getResources().getString(R.string.menuList).equalsIgnoreCase((String) item.getTitle()))
                 {
-                    finish();
+                    finish(); //termine l'activité courante
                 }
+            //si nous avons clicker sur le bouton filter
             else if (getResources().getString(R.string.menuFilter).equalsIgnoreCase((String) item.getTitle()))
                 {
-                    finish();
+                    finish(); //termine l'activité courante
                 }
             else
                 return false;
             return true;
         };
-        
+    
+        /**
+         * A la création de la page curve
+         *
+         * @param savedInstanceState état de l'instance
+         */
         @Override protected void onCreate(Bundle savedInstanceState)
             {
                 super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_curve);
+                setContentView(R.layout.activity_curve); // créer le lien avec le fichier xml associé
                 
-                //System.out.println("koala rouge : " + new Gson().fromJson(getIntent().getStringExtra("selectedGraph"),Graph.class));
-                //selectedGraph = new Gson().fromJson(getIntent().getStringExtra("selectedGraph"),Graph.class);
-                selectedGraph = FilterActivity.selectedGraph;
-                selectedGraph.create_chart(findViewById(R.id.chart), this);
-                selectedGraph.show();
+                selectedGraph = FilterActivity.selectedGraph; // récupère le graphique dans Filter
+                selectedGraph.create_chart(findViewById(R.id.chart), this); // créer le graphique et le lie à la page
+                selectedGraph.show(); // affiche le graphique
                 
-                androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-                setSupportActionBar(toolbar);
-                setToolbarTitle(selectedGraph.getName());
-                
-                Button btnZoomAdd = findViewById(R.id.btnZoomAdd);
-                btnZoomAdd.setOnClickListener(view ->
+                androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);  //créer la barre du haut d'écran et le lie à celui de la vue
+                setSupportActionBar(toolbar); //ajouter cette barre à l'activité courante
+                setToolbarTitle(selectedGraph.getName());  //définir le nom du graphique séléctionné
+    
+                //si on click sur le bouton de zoom avant
+                findViewById(R.id.btnZoomAdd).setOnClickListener(view ->
                 {
                     System.out.println("Zoom IN");
                     selectedGraph.zoomIn();
                 });
-                Button btnZoomLess = findViewById(R.id.btnZoomLess);
-                btnZoomLess.setOnClickListener(view ->
+    
+                //si on click sur le bouton de zoom arrière
+                findViewById(R.id.btnZoomLess).setOnClickListener(view ->
                 {
                     System.out.println("Zoom OUT");
                     selectedGraph.zoomOut();
                 });
-                
-                BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-                bottomNav.setOnNavigationItemSelectedListener(navListener);
+    
+                //ajout de l'évenement de click sur la barre de navigation
+                ((BottomNavigationView) findViewById(R.id.bottom_navigation)).setOnNavigationItemSelectedListener(navListener);
                 
             }
-        
+    
+        /**
+         * Définit le titre de la barre du haut
+         *
+         * @param title titre à insérer
+         */
         public void setToolbarTitle(String title)
             {
+                //si le titre est valide on met à jour le titre de la page
                 Objects.requireNonNull(getSupportActionBar()).setTitle("Selected Graph : " + title);
             }
     }
